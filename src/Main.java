b20f20d1f8405f2e82f874e72f513a01daa18ef1
CAYUNDA_IT2E <markcayunda06>
2026-08-28 09:54:21 +0800

    activity ay

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 public static void main (String[] args ){
     Scanner sc = new Scanner (System.in);
+
     String name, gender;
     int byear;
+
     System.out.print("Enter name:");
     name = sc.nextLine();
     System.out.print("Enter birth year:");
     byear = sc.nextInt();
     System.out.print("Enter gender: ");
     gender = sc.next();
+
     System.out.println("Hello "+name+"! Welcome to java!");
     System.out.println("you were born in "+byear+" and your gender is " +gender);
 }
